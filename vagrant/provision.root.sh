d249f0479ed06563853534263ad2d28ea54a3a06
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2019-07-21 19:15:40 +0900

    パッケージをインストール

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -6,3 +6,13 @@ sudo  timedatectl  set-timezone Asia/Tokyo
 
 sudo  apt  update
 sudo  apt  install -y  build-essential
+
+# Install .Net Core SDK
+wget  -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb  \
+    -O packages-microsoft-prod.deb
+sudo  dpkg  -i packages-microsoft-prod.deb
+
+sudo  apt  update
+sudo  apt  install -y    \
+    apt-transport-https  \
+    dotnet-sdk-2.2
