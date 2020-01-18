9c169302176521a787c6bd8c25e0aad77900ba60
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-01-18 16:54:29 +0900

    パッケージ更新の設定を変更

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -4,10 +4,11 @@ echo  Provisioning $HOSTNAME
 
 sudo  timedatectl  set-timezone Asia/Tokyo
 
+sudo  apt  update
+
 export DEBIAN_FRONTEND=noninteractive
 sudo  -E  apt  install -y  libssl1.1
 
-sudo  apt  update
 sudo  apt  install -y  build-essential      \
     emacs  git  mercurial
 
