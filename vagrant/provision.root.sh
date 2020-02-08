bbf72190eebb31a017193b6db345842110cfd990
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2020-02-08 20:05:33 +0900

    インストールするパッケージを見直し

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -9,8 +9,10 @@ sudo  apt  update
 export DEBIAN_FRONTEND=noninteractive
 sudo  -E  apt  install -y  libssl1.1
 
-sudo  apt  install -y  build-essential      \
-    emacs  git  mercurial
+sudo  apt  install -y  build-essential                          \
+    emacs  git  mercurial  make  time  sudo                     \
+    language-pack-ja-base  language-pack-ja                     \
+    fcitx  fcitx-mozc  ibus-mozc
 
 # Install .Net Core SDK
 wget  -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb  \
@@ -23,6 +25,16 @@ sudo  apt  install -y    \
     dotnet-sdk-3.1
 
 # clean up
+sudo  apt  -y  --purge  autoremove
+sudo  apt  autoclean
+sudo  apt  clean
+
 sudo  rm -f  /var/lib/apt/lists/lock
 sudo  rm -f  /var/cache/apt/archives/lock
 sudo  rm -f  /var/lib/dpkg/lock
+
+sudo  rm  -rf  /tmp/*
+sudo  dd  if=/dev/zero  of=zero  bs=4k  || :
+sudo  rm  -f   zero
+
+history  -c
